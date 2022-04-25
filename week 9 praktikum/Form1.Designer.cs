f16fb50c51fc76bb94c3f6d607a8f6456bef36e4
timotius septian <tseptian@student.ciputra.ac.id>
2022-04-25 10:25:04 +0700

    hbvhu

--- week 9 praktikum/Form1.Designer.cs
+++ week 9 praktikum/Form1.Designer.cs
@@ -44,6 +44,18 @@ namespace week_9_praktikum
             this.lbl_capacity = new System.Windows.Forms.Label();
             this.lbl_namastadium = new System.Windows.Forms.Label();
             this.lbl_namacapacity = new System.Windows.Forms.Label();
+            this.btn_check = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Minutes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnplayername1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columntipe1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnplayername2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columntipe2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbl_tanggal = new System.Windows.Forms.Label();
+            this.lbl_skor = new System.Windows.Forms.Label();
+            this.lbl_hasiltanggal = new System.Windows.Forms.Label();
+            this.lbl_hasilskor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // cBox_home
@@ -150,7 +162,7 @@ namespace week_9_praktikum
             // lbl_stadium
             // 
             this.lbl_stadium.AutoSize = true;
-            this.lbl_stadium.Location = new System.Drawing.Point(257, 253);
+            this.lbl_stadium.Location = new System.Drawing.Point(302, 211);
             this.lbl_stadium.Name = "lbl_stadium";
             this.lbl_stadium.Size = new System.Drawing.Size(71, 17);
             this.lbl_stadium.TabIndex = 11;
@@ -159,7 +171,7 @@ namespace week_9_praktikum
             // lbl_capacity
             // 
             this.lbl_capacity.AutoSize = true;
-            this.lbl_capacity.Location = new System.Drawing.Point(257, 299);
+            this.lbl_capacity.Location = new System.Drawing.Point(302, 257);
             this.lbl_capacity.Name = "lbl_capacity";
             this.lbl_capacity.Size = new System.Drawing.Size(70, 17);
             this.lbl_capacity.TabIndex = 12;
@@ -168,7 +180,7 @@ namespace week_9_praktikum
             // lbl_namastadium
             // 
             this.lbl_namastadium.AutoSize = true;
-            this.lbl_namastadium.Location = new System.Drawing.Point(352, 253);
+            this.lbl_namastadium.Location = new System.Drawing.Point(397, 211);
             this.lbl_namastadium.Name = "lbl_namastadium";
             this.lbl_namastadium.Size = new System.Drawing.Size(31, 17);
             this.lbl_namastadium.TabIndex = 13;
@@ -177,17 +189,121 @@ namespace week_9_praktikum
             // lbl_namacapacity
             // 
             this.lbl_namacapacity.AutoSize = true;
-            this.lbl_namacapacity.Location = new System.Drawing.Point(352, 299);
+            this.lbl_namacapacity.Location = new System.Drawing.Point(397, 257);
             this.lbl_namacapacity.Name = "lbl_namacapacity";
             this.lbl_namacapacity.Size = new System.Drawing.Size(31, 17);
             this.lbl_namacapacity.TabIndex = 14;
             this.lbl_namacapacity.Text = "N/A";
             // 
+            // btn_check
+            // 
+            this.btn_check.Location = new System.Drawing.Point(337, 302);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(75, 23);
+            this.btn_check.TabIndex = 15;
+            this.btn_check.Text = "Check";
+            this.btn_check.UseVisualStyleBackColor = true;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Minutes,
+            this.Columnplayername1,
+            this.Columntipe1,
+            this.Columnplayername2,
+            this.Columntipe2});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 470);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(680, 358);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Minutes
+            // 
+            this.Minutes.HeaderText = "Minutes";
+            this.Minutes.MinimumWidth = 6;
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Width = 125;
+            // 
+            // Columnplayername1
+            // 
+            this.Columnplayername1.HeaderText = "Player Name 1";
+            this.Columnplayername1.MinimumWidth = 6;
+            this.Columnplayername1.Name = "Columnplayername1";
+            this.Columnplayername1.Width = 125;
+            // 
+            // Columntipe1
+            // 
+            this.Columntipe1.HeaderText = "Tipe 1";
+            this.Columntipe1.MinimumWidth = 6;
+            this.Columntipe1.Name = "Columntipe1";
+            this.Columntipe1.Width = 125;
+            // 
+            // Columnplayername2
+            // 
+            this.Columnplayername2.HeaderText = "Player Name 2";
+            this.Columnplayername2.MinimumWidth = 6;
+            this.Columnplayername2.Name = "Columnplayername2";
+            this.Columnplayername2.Width = 125;
+            // 
+            // Columntipe2
+            // 
+            this.Columntipe2.HeaderText = "Tipe 2";
+            this.Columntipe2.MinimumWidth = 6;
+            this.Columntipe2.Name = "Columntipe2";
+            this.Columntipe2.Width = 125;
+            // 
+            // lbl_tanggal
+            // 
+            this.lbl_tanggal.AutoSize = true;
+            this.lbl_tanggal.Location = new System.Drawing.Point(305, 348);
+            this.lbl_tanggal.Name = "lbl_tanggal";
+            this.lbl_tanggal.Size = new System.Drawing.Size(68, 17);
+            this.lbl_tanggal.TabIndex = 17;
+            this.lbl_tanggal.Text = "Tanggal :";
+            // 
+            // lbl_skor
+            // 
+            this.lbl_skor.AutoSize = true;
+            this.lbl_skor.Location = new System.Drawing.Point(308, 388);
+            this.lbl_skor.Name = "lbl_skor";
+            this.lbl_skor.Size = new System.Drawing.Size(65, 17);
+            this.lbl_skor.TabIndex = 18;
+            this.lbl_skor.Text = "Skor      :";
+            // 
+            // lbl_hasiltanggal
+            // 
+            this.lbl_hasiltanggal.AutoSize = true;
+            this.lbl_hasiltanggal.Location = new System.Drawing.Point(380, 348);
+            this.lbl_hasiltanggal.Name = "lbl_hasiltanggal";
+            this.lbl_hasiltanggal.Size = new System.Drawing.Size(28, 17);
+            this.lbl_hasiltanggal.TabIndex = 19;
+            this.lbl_hasiltanggal.Text = ".....";
+            this.lbl_hasiltanggal.Click += new System.EventHandler(this.lbl_hasiltanggal_Click);
+            // 
+            // lbl_hasilskor
+            // 
+            this.lbl_hasilskor.AutoSize = true;
+            this.lbl_hasilskor.Location = new System.Drawing.Point(375, 387);
+            this.lbl_hasilskor.Name = "lbl_hasilskor";
+            this.lbl_hasilskor.Size = new System.Drawing.Size(0, 17);
+            this.lbl_hasilskor.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 840);
+            this.Controls.Add(this.lbl_hasilskor);
+            this.Controls.Add(this.lbl_hasiltanggal);
+            this.Controls.Add(this.lbl_skor);
+            this.Controls.Add(this.lbl_tanggal);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_check);
             this.Controls.Add(this.lbl_namacapacity);
             this.Controls.Add(this.lbl_namastadium);
             this.Controls.Add(this.lbl_capacity);
@@ -206,6 +322,7 @@ namespace week_9_praktikum
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +345,17 @@ namespace week_9_praktikum
         private System.Windows.Forms.Label lbl_capacity;
         private System.Windows.Forms.Label lbl_namastadium;
         private System.Windows.Forms.Label lbl_namacapacity;
+        private System.Windows.Forms.Button btn_check;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Minutes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnplayername1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columntipe1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnplayername2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columntipe2;
+        private System.Windows.Forms.Label lbl_tanggal;
+        private System.Windows.Forms.Label lbl_skor;
+        private System.Windows.Forms.Label lbl_hasiltanggal;
+        private System.Windows.Forms.Label lbl_hasilskor;
     }
 }
 
--- week 9 praktikum/Form1.cs
+++ week 9 praktikum/Form1.cs
@@ -31,13 +31,22 @@ namespace week_9_praktikum
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            DataTable MnChome = new DataTable();
-            sqlQuery = "SELECT m.manager_name as `Manager Name`, p.player_name as `Captain` FROM manager m, player p, team t WHERE m.manager_id = t.manager_id and t.captain_id = p.player_id and t.team_id = '" + cBox_home.SelectedValue.ToString() + "'";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(MnChome);
-            lbl_namamanager1.Text = MnChome.Rows[0]["Manager Name"].ToString();
-            lbl_namacaptain1.Text = MnChome.Rows[0]["Captain"].ToString();
+            try
+            {
+                DataTable MnChome = new DataTable();
+                sqlQuery = "SELECT m.manager_name as `Manager Name`, p.player_name as `Captain` FROM manager m, player p, team t WHERE m.manager_id = t.manager_id and t.captain_id = p.player_id and t.team_id = '" + cBox_home.SelectedValue.ToString() + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(MnChome);
+                lbl_namamanager1.Text = MnChome.Rows[0]["Manager Name"].ToString();
+                lbl_namacaptain1.Text = MnChome.Rows[0]["Captain"].ToString();
+            }
+            catch (Exception)
+            {
+
+                
+            }
+          
 
         }
 
@@ -55,10 +64,10 @@ namespace week_9_praktikum
             cBox_home.DataSource = dtPlayerHome;
             cBox_home.DisplayMember = "team_name";
             cBox_home.ValueMember = "team_id";
-
+            cBox_away.DataSource = dtPlayerAway;
             cBox_away.DisplayMember = "team_name";
             cBox_away.ValueMember = "team_id";
-            cBox_away.DataSource = dtPlayerAway;
+            
         
             
         }
@@ -70,20 +79,65 @@ namespace week_9_praktikum
 
         private void cBox_away_SelectedIndexChanged(object sender, EventArgs e)
         {
-            DataTable MnCAway = new DataTable();
-            sqlQuery = "SELECT m.manager_name as `Manager Name`, p.player_name as `Captain` FROM manager m, player p, TEAM t WHERE m.manager_id = t.manager_id and t.captain_id = p.player_id and t.team_id = '" + cBox_away.SelectedValue.ToString() + "'";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(MnCAway);
-            lbl_namamanager2.Text = MnCAway.Rows[0]["Manager Name"].ToString();
-            lbl_namacaptain2.Text = MnCAway.Rows[0]["Captain"].ToString();
-            DataTable SnC = new DataTable();
-            sqlQuery = "SELECT CONCAT(t.home_stadium, ', ', t.city) as `Stadium`, t.capacity as `Capacity` FROM TEAM t WHERE t.team_id = '" + cBox_home.SelectedValue.ToString() + "'";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(SnC);
-            lbl_namastadium.Text = SnC.Rows[0]["Stadium"].ToString();
-            lbl_namacapacity.Text = SnC.Rows[0]["Capacity"].ToString();
+            try
+            {
+                DataTable MnCAway = new DataTable();
+                sqlQuery = "SELECT m.manager_name as `Manager Name`, p.player_name as `Captain` FROM manager m, player p, TEAM t WHERE m.manager_id = t.manager_id and t.captain_id = p.player_id and t.team_id = '" + cBox_away.SelectedValue.ToString() + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(MnCAway);
+                lbl_namamanager2.Text = MnCAway.Rows[0]["Manager Name"].ToString();
+                lbl_namacaptain2.Text = MnCAway.Rows[0]["Captain"].ToString();
+                DataTable SnC = new DataTable();
+                sqlQuery = "SELECT CONCAT(t.home_stadium, ', ', t.city) as `Stadium`, t.capacity as `Capacity` FROM TEAM t WHERE t.team_id = '" + cBox_home.SelectedValue.ToString() + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(SnC);
+                lbl_namastadium.Text = SnC.Rows[0]["Stadium"].ToString();
+                lbl_namacapacity.Text = SnC.Rows[0]["Capacity"].ToString();
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+        }
+
+        private void lbl_hasiltanggal_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable tanggalmainskor = new DataTable();
+                sqlQuery = ("SELECT date_format(match_date,\"%e %M %Y\") as 'date', concat(`goal_home`, '-' , `goal_away`) as Goal FROM `match` where team_home = '" + cBox_home.SelectedValue.ToString() + "' and team_away = '" + cBox_away.SelectedValue.ToString() + "'");
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(tanggalmainskor);
+                lbl_hasiltanggal.Text = tanggalmainskor.Rows[0]["date"].ToString();
+                lbl_hasilskor.Text = tanggalmainskor.Rows[0]["Goal"].ToString();
+                DataTable tabel = new DataTable();
+                sqlQuery = "SELECT Minutes.Minutesas menit, if(m.";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(tabel);
+                dataGridView1.DataSource = tabel;
+            }
+            catch (Exception)
+            {
+
+                
+            }
+         
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
